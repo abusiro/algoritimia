c8cd2fd6ea1d59bb85c9b43470e55e6d3d3e6657
abusiro <abusiro@yandex.com>
2026-02-10 21:23:41 +0100

    2 pointers

---
+++ ejercicios/01_two_sum/description.md
@@ -0,0 +1,33 @@
+
+# TWO SUM
+ PROBLEMA (LeetCode #1 - Fácil)
+
+ Dado un array de enteros nums y un entero target, devuelve los índices de los dos números que suman target.
+
+ Suposiciones:
+
+- Existe exactamente una solución
+- No puedes usar el mismo elemento dos veces
+- Puedes devolver la respuesta en cualquier orden
+
+ ## EJEMPLOS:
+ ### Ejemplo 1:
+ ```text
+ Input: nums = [2, 7, 11, 15], target = 9
+
+ Output: [0, 1]
+ Explicación: nums[0] + nums[1] = 2 + 7 = 9
+```
+ ### Ejemplo 2:
+ ```text
+ Input: nums = [3, 2, 4], target = 6
+ Output: [1, 2]
+ Explicación: nums[1] + nums[2] = 2 + 4 = 6
+```
+### Ejemplo 3:
+ ```text
+ Input: nums = [3, 2, 4], target = 6
+ Output: [1, 2]
+ Explicación: nums[1] + nums[2] = 2 + 4 = 6
+```
+
--- ejercicios/01_two_sum/src/main.rs
+++ ejercicios/01_two_sum/src/main.rs
@@ -1,48 +1,33 @@
-
-// EJERCICIO: TWO SUM
-// PROBLEMA (LeetCode #1 - Fácil)
-//
-// Dado un array de enteros nums y un entero target, devuelve los índices de los dos números que suman target.
-//
-// Suposiciones:
-//
-//     Existe exactamente una solución
-//
-//     No puedes usar el mismo elemento dos veces
-//
-//     Puedes devolver la respuesta en cualquier orden
-//
-// EJEMPLOS:
-// Ejemplo 1:
-// rust
-//
-// Input: nums = [2, 7, 11, 15], target = 9
-// Output: [0, 1]
-// Explicación: nums[0] + nums[1] = 2 + 7 = 9
-//
-// Ejemplo 2:
-// rust
-//
-// Input: nums = [3, 2, 4], target = 6
-// Output: [1, 2]
-// Explicación: nums[1] + nums[2] = 2 + 4 = 6
-//
-// Ejemplo 3:
-// rust
-//
-// Input: nums = [3, 3], target = 6
-// Output: [0, 1]
-//
+// solution brute force
 pub fn two_sum_brute(nums: Vec<i32>, target: i32) -> Vec<i32> {
     for i in 0..nums.len() {
         for j in (i + 1)..nums.len() {
             if nums[i] + nums[j] == target {
-                return vec![i as i32, j as i32];
             }
+            return vec![nums[i] as i32, nums[j] as i32];
         }
     }
     vec![]
 }
+// it only works if nums is sorted
+pub fn two_sum_two_pointers(nums: Vec<i32>, target: i32) -> Vec<i32> {
+
+    let mut left = 0;   
+    let mut right = nums.len() - 1;
+    while left < right {
+        let sum = nums[left] + nums[right];
+        if sum == target {
+            return vec![nums[left] as i32, nums[right] as i32];
+        } else if sum < target {
+            left += 1
+        } else if sum > target {
+            right -= 1
+        }
+    }
+
+    vec![]
+}
+
 fn main() {
     // Ejemplos de prueba
     let test_cases = vec![
@@ -52,8 +37,16 @@ fn main() {
         (vec![1, 5, 3, 7, 2], 10, vec![2, 3]),
     ];
 
-    for (nums, target, expected) in test_cases {
+    for (nums, target, expected) in test_cases.clone() {
         let result = two_sum_brute(nums.clone(), target);
+        println!("nums: {:?}, target: {}", nums, target);
+        println!("  Esperado: {:?}", expected);
+        println!("  Resultado: {:?}", result);
+        println!("  ✓ Correcto: {}\n", result == expected);
+    }
+
+    for (nums, target, expected) in test_cases.clone() {
+        let result = two_sum_two_pointers(nums.clone(), target);
         println!("nums: {:?}, target: {}", nums, target);
         println!("  Esperado: {:?}", expected);
         println!("  Resultado: {:?}", result);
